c6f072ad81fb069cbd50a0e7c026b6a86557ddec
MaximKuzakov <mks.sfu@yandex.ru>
2022-10-04 14:12:07 +0300

    Example How to make PageObjectModel

--- Section4/test_main_page.py
+++ Section4/test_main_page.py
@@ -8,6 +8,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    # link2 = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    # page2 = LoginPage(browser, link2)
-    # page2.open()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
