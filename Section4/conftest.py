3f975270464ea1b10438e380c430cb21212c1a0d
MaximKuzakov <mks.sfu@yandex.ru>
2022-10-04 09:53:22 +0300

    ЫуÐsetup env for project Section4

--- Section4/conftest.py
+++ Section4/conftest.py
@@ -1,6 +1,17 @@
-def main():
-    pass
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-if __name__ == '__main__':
-    main()
+def pytest_addoption(parser):
+    parser.addoption('--language')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
